26b0e712da9c9c333b1f43855781fc6359207d3e
devcom33 <mouad.elb06@gmail.com>
2024-01-01 19:13:59 +0100

    Adjust window size and optimize background images

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ class Face_Recognition_System:
     def __init__(self, root):
         self.root = root
 
-        self.root.geometry('1530x790+0+0')
+        self.root.geometry('1400x930+100+50')
         self.root.title('Système de présence intelligent')
         self.root.configure(bg="#fff")
-        self.root.resizable(False, False)
+        self.root.resizable(True, True)
 
 
 
@@ -32,7 +32,7 @@ class Face_Recognition_System:
 
 
         # Charger l'image que vous souhaitez utiliser pour l'icône
-        icon = tkinter.PhotoImage(file="C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\back.png")
+        icon = tkinter.PhotoImage(file="images\\back.png")
 
         # Définir l'icône de la fenêtre principale
         root.iconphoto(True, icon)
@@ -61,33 +61,33 @@ class Face_Recognition_System:
 
         ###########################################################################################################################
 
-        img = Image.open("C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\po\\bnbn.jpg")
-        img = img.resize((1530, 790),  resample=Image.LANCZOS)
+        img = Image.open("images\\po\\bnbn.jpg")
+        img = img.resize((1400, 930),  resample=Image.LANCZOS)
         self.photoimg = ImageTk.PhotoImage(img)
 
 
 
         f_lbl = Label(self.root, image=self.photoimg)
-        f_lbl.place(x=0, y=0, width=1530, height=790)
+        f_lbl.place(x=0, y=0, width=1400, height=930)
 
         title_lbl = Label(self.root, text="SYSTEME DE PRESENCE INTELLIGENT ", font=("Microsoft Himalaya", 40, "bold"),
                           bg="#091821", fg="#57a1f8")
-        title_lbl.place(x=0, y=50, width=1530, height=50)
+        title_lbl.place(x=0, y=50, width=1400, height=50)
 
-        img1 = Image.open("C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\po\\yara.png")
+        img1 = Image.open("images\\po\\yara.png")
         img1 = img1.resize((220, 200),  resample=Image.LANCZOS)
         self.photoimg1 = ImageTk.PhotoImage(img1)
 
         b1 = Button(self.root,border=0, image=self.photoimg1)
-        b1.place(x=330, y=250, width=220, height=200)
+        b1.place(x=250, y=250, width=220, height=200)
 
         #Button(frame, width=39, pady=7, text='Sign in', bg='#57a1f8', fg='white', border=0, command=signin).place(x=35,y=204)
 
         b1_1 = Button(self.root, width=39, pady=7,border=0, text="gestion des etudiants", cursor="hand2",
                        bg='#57a1f8', fg='white', font=(0, 10,"bold"), command=gestion)
-        b1_1.place(x=330, y=460, width=220, height=50)
+        b1_1.place(x=250, y=460, width=220, height=50)
 
-        img3 = Image.open("C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\po\\this.jpg")
+        img3 = Image.open("images\\po\\this.jpg")
         img3 = img3.resize((220, 200), resample=Image.LANCZOS)
         self.photoimg3 = ImageTk.PhotoImage(img3)
 
@@ -100,7 +100,7 @@ class Face_Recognition_System:
 
         ##########################################################################################################
 
-        '''img6 = Image.open("C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\aide.jpg")
+        '''img6 = Image.open("images\\aide.jpg")
         img6 = img6.resize((220, 200), resample=Image.LANCZOS)
         self.photoimg6 = ImageTk.PhotoImage(img6)
 
@@ -113,7 +113,7 @@ class Face_Recognition_System:
 
         ##########################################################################################################
 
-        img2 = Image.open("C:\\Users\\viet\\PycharmProjects\\mongodb\\images\\emp.jpg")
+        img2 = Image.open("images\\emp.jpg")
         img2 = img2.resize((220, 200), resample=Image.LANCZOS)
         self.photoimg2 = ImageTk.PhotoImage(img2)
 
